254dd6ba6d6272df358f8e93c33c8c91803e8496
Robert Carlsen <rwcarlsen@gmail.com>
2015-05-07 20:28:43 -0600

    roughing out support for proxying cloudlus worker requests through an intermediate proxy node to get around network restrictions on execute nodes in clusters

--- revproxy.go
+++ revproxy.go
@@ -2,41 +2,33 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"strings"
 )
 
 var addr = flag.String("addr", "", "address to listen on")
+var target = flag.String("target", "", "address to forward to")
 
-var proxys = map[string]*httputil.ReverseProxy{}
-
-// positional args are [<subdomain> <target-addr>]...
 func main() {
 	flag.Parse()
-
-	for i := 0; i < len(flag.Args()); i += 2 {
-		subdomain := flag.Arg(i)
-		target := flag.Arg(i + 1)
-		remote, err := url.Parse("http://" + target)
-		if err != nil {
-			log.Fatal(err)
-		}
-		proxys[subdomain] = httputil.NewSingleHostReverseProxy(remote)
-	}
-
-	http.HandleFunc("/", handler)
-	err := http.ListenAndServe(*addr, nil)
+	remote, err := url.Parse("http://" + *target)
 	if err != nil {
 		log.Fatal(err)
 	}
-}
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	subdomain := strings.Split(r.Host, ".")[0]
-	if proxy, ok := proxys[subdomain]; ok {
-		proxy.ServeHTTP(w, r)
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+	proxy.Director = func(req *http.Request) {
+		fmt.Println("serving request ", req.URL)
+		req.URL.Scheme = remote.Scheme
+		req.URL.Host = remote.Host
+	}
+
+	http.Handle("/", proxy)
+	err = http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
